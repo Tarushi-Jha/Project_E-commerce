9171cb1a297d9736238a8ff589bf0054890d170c
Shiksha1501 <shikshavishwakarma1501@gmail.com>
2025-01-22 00:11:42 +0530

    Created Get single product by id

--- Backend/controllers/productControllers.js
+++ Backend/controllers/productControllers.js
@@ -2,7 +2,7 @@ import Product from "../models/product.js";
 
 // Create new Product   =>  /api/v1/products
 export const getProducts = async (req, res) => {
-    
+
   const products = await Product.find();
   res.status(200).json({
     products,
@@ -12,6 +12,21 @@ export const getProducts = async (req, res) => {
 // Create new Product   =>  /api/v1/admin/products
 export const newProduct = async (req, res) => {
   const product = await Product.create(req.body);
+  res.status(200).json({
+    product,
+  });
+};
+
+// Get single product details  => /api/v1/products/:id
+export const getProductDetails = async (req, res) => {
+  const product = await Product.findById(req?.params?.id);
+
+  if (!product) {
+    return res.status(404).json({
+      error: "Product not found",
+    });
+  }
+
   res.status(200).json({
     product,
   });
