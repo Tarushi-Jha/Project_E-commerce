6df3daadf40ef7d85a742bfa146e25fb8cf5bbf6
Shiksha1501 <shikshavishwakarma1501@gmail.com>
2025-01-22 12:38:14 +0530

    Unhandled promise rejection and uncaught exceptions handled

--- Backend/app.js
+++ Backend/app.js
@@ -4,12 +4,20 @@ import dotenv from "dotenv";
 import { connectDatabase } from "./config/dbConnect.js";
 import errorMiddleware from "./middlewares/errors.js";
 
+// Handle Uncaught exceptions
+process.on("uncaughtException", (err) => {
+  console.log(`ERROR: ${err}`);
+  console.log("Shutting down due to uncaught expection");
+  process.exit(1);
+});
+
 dotenv.config({ path: "backend/config/config.env" });
 
 //Connecting to DataBase
 connectDatabase();
 
 app.use(express.json());
+
 
 //Import all routes
 import productRoutes from "./routes/products.js";
@@ -20,8 +28,17 @@ app.use("/api/v1", productRoutes);
 app.use(errorMiddleware);
 
 
-app.listen(process.env.PORT, () => {
+const server = app.listen(process.env.PORT, () => {
   console.log(
     `Server started on PORT: ${process.env.PORT} in ${process.env.NODE_ENV} mode`
   );
+});
+
+//Handle Unhandled Promise rejections
+process.on("unhandledRejection", (err) => {
+  console.log(`ERROR: ${err}`);
+  console.log("Shutting down server due to Unhandled Promise Rejection");
+  server.close(() => {
+    process.exit(1);
+  });
 });
