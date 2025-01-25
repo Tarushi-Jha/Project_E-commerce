a00c832351151cf5e13f263486ab35384f6c7847
Yaswitha1810 <yaswitha.1810@gmail.com>
2025-01-25 18:39:19 +0530

    Added all Admin Routes

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -7,10 +7,15 @@ import {
   resetPassword,
   getUserProfile,
   updatePassword,
+  updateProfile,
+  allUsers,
+  getUserDetails,
+  updateUser,
+  deleteUser,
 } from "../controllers/authControllers.js";
 const router = express.Router();
 
-import { isAuthenticatedUser } from "../middlewares/auth.js";
+import { authorizeRoles, isAuthenticatedUser } from "../middlewares/auth.js";
 
 router.route("/register").post(registerUser);
 router.route("/login").post(loginUser);
@@ -18,8 +23,18 @@ router.route("/logout").get(logout);
 
 router.route("/password/forgot").post(forgotPassword);
 router.route("/password/reset/:token").post(resetPassword);
+router.route("/password/update").put(isAuthenticatedUser, updatePassword);
 
 router.route("/me").get(isAuthenticatedUser, getUserProfile);
-router.route("/password/update").put(isAuthenticatedUser, updatePassword);
+router.route("/me/update").put(isAuthenticatedUser, updateProfile);
+
+router
+  .route("/admin/users")
+  .get(isAuthenticatedUser, authorizeRoles("admin"), allUsers);
+router
+  .route("/admin/users")
+  .get(isAuthenticatedUser, authorizeRoles("admin"), getUserDetails)
+  .put(isAuthenticatedUser, authorizeRoles("admin"), updateUser)
+  .delete(isAuthenticatedUser, authorizeRoles("admin"), deleteUser);
 
 export default router;
