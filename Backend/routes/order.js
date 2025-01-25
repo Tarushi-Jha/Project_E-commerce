12fb20c14eedf8199486c8bb1497df417fae9704
Yaswitha1810 <yaswitha.1810@gmail.com>
2025-01-25 23:31:16 +0530

    Order Routes Added

--- Backend/routes/order.js
+++ Backend/routes/order.js
@@ -2,8 +2,10 @@ import express from "express";
 const router = express.Router();
 
 import { isAuthenticatedUser } from "../middlewares/auth.js";
-import { newOrder } from "../controllers/orderContollers.js";
+import { getOrderDetails, myOrders, newOrder } from "../controllers/orderContollers.js";
 
 router.route("/orders/new").post(isAuthenticatedUser, newOrder);
+router.route("/orders/:id").get(isAuthenticatedUser, getOrderDetails);
+router.route("/me/orders").get(isAuthenticatedUser, myOrders);
 
 export default router;
