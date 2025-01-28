44fd8324d6bb97ce57a58f33d54ea33af315b220
Tarushi <tarushijha97@gmail.com>
2025-01-29 00:28:20 +0530

    added pagination and keyword search in frontend

--- Backend/app.js
+++ Backend/app.js
@@ -11,7 +11,7 @@ process.on("uncaughtException", (err) => {
   process.exit(1);
 });
 
-dotenv.config({ path: "config/.env" });
+dotenv.config({ path: "Backend/config/.env" });
 
 //Connecting to DataBase
 connectDatabase();
--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -4,37 +4,55 @@ import { useGetProductsQuery } from "../redux/api/productApi";
 import ProductItem from "./product/ProductItem";
 import Loader from "./layout/Loader";
 import toast from "react-hot-toast";
+import CustomPagination from "./layout/CustiomPagination";
+import { useSearchParams } from "react-router-dom";
 
 const Home = () => {
+  let [searchParams] = useSearchParams();
+  const page = searchParams.get("page") || 1;
+  const keyword = searchParams.get("keyword") || "";
+  const params = { page, keyword };
+  const { data, isLoading, error, isError } = useGetProductsQuery(params);
 
-  const { data, isLoading, error, isError } = useGetProductsQuery();
-
-  useEffect(()=>{
-    if(isError) {
-        toast.error(error?.data?.message);
+  useEffect(() => {
+    if (isError) {
+      toast.error(error?.data?.message);
     }
-  }, [isError])
+  }, [isError]);
 
-  if (isLoading) return <Loader/>;
+  const columnSize = keyword ? 4 : 3;
+
+  if (isLoading) return <Loader />;
 
   return (
     <>
       <MetaData title={"Home"} />
-        <div className="row">
-            <div className="col-6 col-md-12">
-            <h1 id="products_heading" className="text-secondary">
-                Latest Products
-            </h1>
+      <div className="row">
+        {keyword && (
+          <div className="col-6 col-md-3 mt-5">
+            <p>FILTERS</p>
+          </div>
+        )}
+        <div className={keyword ? "col-6 col-md-9" : "col-6 col-md-12"}>
+          <h1 id="products_heading" className="text-secondary">
+            {keyword
+              ? `${data?.products?.length} Products found with keyword: ${keyword}`
+              : "Latest Products"}
+          </h1>
 
-            <section id="products" className="mt-5">
-                <div className="row">
-                {data?.products?.map((product) => (
-                    <ProductItem product={product} />
-                ))}
-                </div>
-            </section>
+          <section id="products" className="mt-5">
+            <div className="row">
+              {data?.products?.map((product) => (
+                <ProductItem product={product} columnSize={columnSize} />
+              ))}
             </div>
+          </section>
+          <CustomPagination
+            resPerPage={data?.resPerPage}
+            filteredProductsCount={data?.filteredProductsCount}
+          />
         </div>
+      </div>
     </>
   );
 };
