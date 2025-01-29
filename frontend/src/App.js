467d33bf7c1b9e9b01caaf8b39cb3044bb4c8021
Tarushi <tarushijha97@gmail.com>
2025-01-29 12:23:31 +0530

    added login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,30 @@
 import "./App.css";
 
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
-import { Toaster } from 'react-hot-toast';
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
 
 import Footer from "./components/layout/Footer";
 import Header from "./components/layout/Header";
 import Home from "./components/Home";
+import Login from "./components/auth/login";
 import ProductDetails from "./components/product/productDetails";
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Toaster position="top-center" /> 
-        <Header/>
-        
+        <Toaster position="top-center" />
+        <Header />
+
         <div className="container">
           <Routes>
-            <Route path="/" element={<Home/>} />
+            <Route path="/" element={<Home />} />
             <Route path="/product/:id" element={<ProductDetails />} />
+            <Route path="/login" element={<Login />} />
           </Routes>
         </div>
-        
-        <Footer/>
+
+        <Footer />
       </div>
     </Router>
   );
