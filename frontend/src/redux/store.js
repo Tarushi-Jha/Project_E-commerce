7cca0b478b720160276ff2bb05c5d9e2abddd993
Shiksha1501 <shikshavishwakarma1501@gmail.com>
2025-01-29 17:07:56 +0530

    Added load logged in user in state

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,12 +1,20 @@
 import { configureStore } from "@reduxjs/toolkit";
+import useReducer from "./features/userSlice";
 import { productApi } from "./api/productApi";
 import { authApi } from "./api/authApi";
+import { userApi } from "./api/userApi";
 
 export const store = configureStore({
   reducer: {
+    auth: useReducer,
     [productApi.reducerPath]: productApi.reducer,
     [authApi.reducerPath]: authApi.reducer,
+    [userApi.reducerPath]: userApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat([productApi.middleware, authApi.middleware]),
+    getDefaultMiddleware().concat([
+      productApi.middleware,
+      authApi.middleware,
+      userApi.middleware,
+    ]),
 });
